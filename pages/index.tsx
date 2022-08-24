7de921239434a8c45c849ed31f1e7e5b0d445683
Maythee Anegboonlap <contact@llun.dev>
2022-08-24 21:13:59 +0200

    Init

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,7 +1,42 @@
-import type { NextPage } from 'next'
-import Head from 'next/head'
-import Image from 'next/image'
-import styles from '../styles/Home.module.css'
+import type { NextPage } from "next";
+import Head from "next/head";
+import Image from "next/image";
+import { FC, useRef, useEffect } from "react";
+import mapboxgl from "mapbox-gl";
+
+import "mapbox-gl/dist/mapbox-gl.css";
+import styles from "../styles/Home.module.css";
+
+const MAPBOX_PUBLIC_KEY =
+  "pk.eyJ1IjoibGx1biIsImEiOiJja2FqN2k2djIwNDU5MnlvNjR4YXRrMzFsIn0.Oir7SYHkVKBlgbPHldtRGQ";
+
+const RideMap: FC = () => {
+  const mapEl = useRef<HTMLDivElement>(null);
+  mapboxgl.accessToken = MAPBOX_PUBLIC_KEY;
+
+  useEffect(() => {
+    const zoomLevel = (height?: number) => {
+      switch (height) {
+        case 250:
+          return 6.8;
+        case 400:
+          return 7;
+        default:
+          return 7.25;
+      }
+    };
+
+    new mapboxgl.Map({
+      container: "map",
+      style: "mapbox://styles/mapbox/light-v10",
+      center: [4.902218907700037, 52.37208643243944],
+      zoom: zoomLevel(mapEl?.current?.offsetHeight),
+      minZoom: 6.8,
+    });
+  }, []);
+
+  return <div ref={mapEl} id="map" className={styles.map} />;
+};
 
 const Home: NextPage = () => {
   return (
@@ -14,43 +49,10 @@ const Home: NextPage = () => {
 
       <main className={styles.main}>
         <h1 className={styles.title}>
-          Welcome to <a href="https://nextjs.org">Next.js!</a>
+          Welcome to <a href="#">Route Cleaner!</a>
         </h1>
 
-        <p className={styles.description}>
-          Get started by editing{' '}
-          <code className={styles.code}>pages/index.tsx</code>
-        </p>
-
-        <div className={styles.grid}>
-          <a href="https://nextjs.org/docs" className={styles.card}>
-            <h2>Documentation &rarr;</h2>
-            <p>Find in-depth information about Next.js features and API.</p>
-          </a>
-
-          <a href="https://nextjs.org/learn" className={styles.card}>
-            <h2>Learn &rarr;</h2>
-            <p>Learn about Next.js in an interactive course with quizzes!</p>
-          </a>
-
-          <a
-            href="https://github.com/vercel/next.js/tree/canary/examples"
-            className={styles.card}
-          >
-            <h2>Examples &rarr;</h2>
-            <p>Discover and deploy boilerplate example Next.js projects.</p>
-          </a>
-
-          <a
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-            className={styles.card}
-          >
-            <h2>Deploy &rarr;</h2>
-            <p>
-              Instantly deploy your Next.js site to a public URL with Vercel.
-            </p>
-          </a>
-        </div>
+        <RideMap />
       </main>
 
       <footer className={styles.footer}>
@@ -59,14 +61,14 @@ const Home: NextPage = () => {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Powered by{' '}
+          Powered by{" "}
           <span className={styles.logo}>
             <Image src="/vercel.svg" alt="Vercel Logo" width={72} height={16} />
           </span>
         </a>
       </footer>
     </div>
-  )
-}
+  );
+};
 
-export default Home
+export default Home;
